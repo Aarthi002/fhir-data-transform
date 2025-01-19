05d9c08a4620f474d7a9b2e523cdb99aa72ac5bd
Aarthi002 <72254890+Aarthi002@users.noreply.github.com>
2025-01-19 17:05:12 +0530

    fix: airflow

--- dags/dags.py
+++ dags/dags.py
@@ -1,19 +1,19 @@
-# spark_dag.py
+# ~/airflow/dags/spark_dag.py
 
-from airflow import DAG 
-from airflow.operators.bash import BashOperator # Updated import path 
+from airflow import DAG
+from airflow.operators.bash import BashOperator
 from datetime import datetime
 
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2023, 1, 1),
-    'retries': 1,
+    'start_date': datetime(2023, 1, 1)
 }
 
-dag = DAG(dag_id='spark_dag', default_args=default_args, schedule='@daily')
+dag = DAG(dag_id='spark_dag', default_args=default_args, schedule_interval='@daily')
 
-spark_job = BashOperator(
-    task_id='spark_job',
-    bash_command='python recon.py',
+run_spark_job = BashOperator(
+    task_id='run_spark_job',
+    bash_command='source /Users/aarthimurali/Documents/GitHub/fhir-data-transform/venv/bin/activate && python /Users/aarthimurali/Documents/GitHub/fhir-data-transform/recon.py',
     dag=dag
 )
+
